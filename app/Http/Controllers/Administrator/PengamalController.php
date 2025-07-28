d2d37952759947ede2944a59d1dd3d86e071c701
sangsakaa <zainuri7172gmail.com>
2025-07-28 20:32:53 +0800

    p

--- app/Http/Controllers/Administrator/PengamalController.php
+++ app/Http/Controllers/Administrator/PengamalController.php
@@ -37,7 +37,7 @@ class PengamalController extends Controller
         // } else {
         //     abort(403, 'Anda tidak memiliki izin untuk mengakses data ini.');
         // }
-        if (Auth::user()->hasRole('admin-provinsi')) {
+        if (Auth::user()->hasRole(['admin-provinsi', 'superAdmin'])) {
             $query = Pengamal::with(['regency', 'district', 'village'])->paginate(10);
         } else {
             abort(403, 'Unauthorized');
