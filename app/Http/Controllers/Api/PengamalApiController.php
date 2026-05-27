ede0fc5471df7af60b858a4d6b525e3069d19a75
sangsaka <zainuri717@gmail.com>
2026-05-27 22:51:28 +0800

    laporan pee kabupaten

--- app/Http/Controllers/Api/PengamalApiController.php
+++ app/Http/Controllers/Api/PengamalApiController.php
@@ -10,17 +10,28 @@ class PengamalApiController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Pengamal::query()->with([
-            'province',
-            'regency',
-            'district',
-            'village'
-        ]);
+        $user = auth()->user();
+
+        abort_unless(
+            $user?->hasAnyRole([
+                'superAdmin',
+                'admin-provinsi',
+                'admin-kabupaten',
+                'admin-kecamatan',
+                'admin-desa',
+            ]),
+            403,
+            'Unauthorized'
+        );
+
+        $query = Pengamal::query()
+            ->with(Pengamal::relations())
+            ->byUserRole($user);
 
         return response()->json([
             'success' => true,
-            'total' => $query->count(),
-            'data' => $query->get(),
+            'total'   => $query->count(),
+            'data'    => $query->get(),
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PengamalApiController;
 
-Route::get('/pengamal', [
-  PengamalApiController::class,
-  'index'
-]);
+Route::middleware([
+  'auth:sanctum',
+  'role:superAdmin|admin-provinsi|admin-kabupaten|admin-kecamatan|admin-desa'
+])->group(function () {
+
+  Route::get(
+    '/pengamal',
+    [PengamalApiController::class, 'index']
+  );
+});
